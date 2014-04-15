7fec4c17c9038ce23b79aab5dab5201b371eb744
Craig <cgleckman@gmail.com>
2014-04-15 15:07:59 -0700

    Kivy Testing
    More Kivy

--- PalaceCardGamePythonWebApp/Kivy/Palace_Kivy.py
+++ PalaceCardGamePythonWebApp/Kivy/Palace_Kivy.py
@@ -1,108 +1,86 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Craig
-#
-# Created:     11/04/2014
-# Copyright:   (c) Craig 2014
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-#Note:
-#   Get Graphics done basically and then upgrade graphics
-#   Dont focus on making it look great at first.
-
-
-
-#What does it need to do? (not in a particular order)
-#
-#   *COMPLETE*1) Display a background card table
-#
-#   2) Area for each player's cards to be displayed, and then display back of
-#   cards if not yours to see
-#       2A) This needs to update based on what cards a person has
-#           2Aa) Card area will display one of each rank of card that a player
-#           has in their hand, then will open an additional menu if more then 1
-#           of that rank exist
-#
-#   3) Area for middle pile
-#
-#   4) Area for each players palace
-#
-#   5) Area for each players topofpalace (which displays ontop of the palace
-#   section)
-#
-#   6) Allow a player to pick a card, show if he has more then 1 of those cards,
-#   and let them choose to play more then 1 if they wish
-#       6A) Play will be done by clicking card, and any additional to play, and
-#       then clicking middle pile.
-
-#Initial Background Creation
-#TODO: Figure out how to move this to .kv file if possible
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.base import runTouchApp
 from kivy.lang import Builder
-from random import random
-from kivy.graphics import Color, Ellipse, Line, Rectangle
-from kivy.uix.button import Button
+
+from kivy.uix.widget import Widget
+
+from kivy.core.window import Window
+from kivy.core.image import Image
+from kivy.properties import ObjectProperty
 
 Builder.load_string('''
-<GridLayout>
-    canvas.before:
-        BorderImage:
-            # BorderImage behaves like the CSS BorderImage
-            border: 10, 10, 10, 10
-            source: 'C:/Users/Craig/Documents/GitHub/Palace/PalaceCardGamePythonWebApp/Kivy/tablecutout.png'
+<Root>:
+    PalaceGame:
+        pos: 800, 600
+    PlayerOneCardHolder:
+        pos: 300, 100
+    PlayerTwoCardHolder:
+        pos: 150, 225
+    PlayerThreeCardHolder:
+        pos: 300, 475
+    PlayerFourCardHolder:
+        pos: 600, 225
+
+<PalaceGame>:
+    Image:
+        source: 'tablecutout.png'
+        allow_stretch: True
+        keep_ratio: False
+        size: 800, 600
+
+<PlayerOneCardHolder>:
+    canvas:
+        Color:
+            rgba: 0, 0, 0, .5
+        Rectangle:
             pos: self.pos
-            size: self.size
+            size: 200, 50
 
+<PlayerTwoCardHolder>:
+    canvas:
+        Color:
+            rgba: 0, 0, 0, .5
+        Rectangle:
+            pos: self.pos
+            size: 50, 200
 
-<PalaceGame>
-    GridLayout:
-        size_hint: .9, .9
-        # helps display exactly center of screen (pos 0-1)
-        pos_hint: {'center_x': .5, 'center_y': .5}
-        rows:1
+<PlayerThreeCardHolder>:
+    canvas:
+        Color:
+            rgba: 0, 0, 0, .5
+        Rectangle:
+            pos: self.pos
+            size: 200, 50
+
+<PlayerFourCardHolder>:
+    canvas:
+        Color:
+            rgba: 0, 0, 0, .5
+        Rectangle:
+            texture: self.card.texture
+            pos: self.pos
+            size: 50, 200
 ''')
-class PalaceGame(FloatLayout):
 
-    def __init__(self, **kwargs):
-        # make sure we aren't overriding any important functionality
-        super(PalaceGame, self).__init__(**kwargs)
+class Root(Widget):
+    pass
 
-        # let's add a Widget to this layout
-        self.add_widget(
-                        Rectangle(
-                                text="Test",
-                                size_hint= (.1, .1),
-                                pos_hint={'center_x':.25,
-                                        'center_y':.25}))
+class PalaceGame(Widget):
+    pass
 
-    #just for testing how kivy functions -- remove for actual game
-    def on_touch_down(self, touch):
-        with self.canvas.before:
-            Color(0, 0, 1)
-            #diameter
-            d = 60.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+class PlayerOneCardHolder(Widget):
+    pass
 
+class PlayerTwoCardHolder(Widget):
+    pass
 
-class PalaceApp(App):
+class PlayerThreeCardHolder(Widget):
+    pass
 
-    def build(self):
-        self.game = game = PalaceGame()
-        game.bind(
-                    size=self._update_rect,
-                    pos=self._update_rect)
-        with game.canvas.before:
-            self.rect = Rectangle(
-                                    size=game.size,
-                                    pos=game.pos)
-        return game
+class PlayerFourCardHolder(Widget):
+    card = ObjectProperty(None)
 
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
+    def __init__(self, **kw):
+        super(PlayerFourCardHolder, self).__init__(**kw)
+        self.card = Image('chips.png')
 
-if __name__ == '__main__':
-    PalaceApp().run()
+runTouchApp(Root())
